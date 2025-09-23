cb35a6efa1df8f109c0eab1576f0478e01587c88
IHYDIAF <joshellangelo@gmail.com>
2025-09-23 18:30:23 -0400

    feat: added two subclasses: savings and checkings

--- BankAccount/BankAccount.py
+++ BankAccount/BankAccount.py
@@ -24,6 +24,35 @@ class BankAccount:
     def print_customer_information(self):
         print(BankAccount.bank_title + "\n" + self.name + "'s account: \nBalance: " + str(self.currentBalance) + "\n")
 
+class SavingsAccount(BankAccount):
+    def __init__(self, name, currentbalance, minimum_balance, interest_rate):
+        self.name = name
+        self.currentBalance = currentbalance
+        self.minimumBalance = minimum_balance
+        self.interest_rate = interest_rate
+
+    def add_interest(self):
+        interest = self.currentBalance * self.interest_rate
+        self.currentBalance += interest
+        print(f"Interest of {interest}$ added. New balance: {self.currentBalance}$\n")
+
+
+class CheckingAccount(BankAccount):
+    def __init__(self, name, currentbalance, minimum_balance, transfer_limit):
+        self.name = name
+        self.currentBalance = currentbalance
+        self.minimumBalance = minimum_balance
+        self.transfer_limit = transfer_limit
+
+    def transfer(self, value, target_account):
+        if value > self.transfer_limit:
+            print(f"Transfer denied: Amount exceeds transfer limit of {self.transfer_limit}$.\n")
+        elif self.currentBalance - value < self.minimumBalance:
+            print("Transfer denied: Insufficient funds for transfer.\n")
+        else:
+            self.currentBalance -= value
+            target_account.currentBalance += value
+            print(f"Transferred {value}$ to {target_account.name}. Your new balance: {self.currentBalance}$\n")
 
 b1 = BankAccount("Josh", 2000, 100)
 b1.print_customer_information()
